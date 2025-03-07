f5dbdccb4e62c69c9c1edde18bea67ac2026c3ef
Ayman Algamal <ayman01287985950@gmail.com>
2025-03-07 02:40:38 +0200

    migrating to jdbc, adding schema.sql file

--- ratings-data-service/src/main/java/com/example/ratingsservice/models/RatingEntry.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/models/RatingEntry.java
@@ -1,22 +1,10 @@
 package com.example.ratingsservice.models;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "ratings")
 public class RatingEntry {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String userId;
-
-    @Column(nullable = false)
     private String movieId;
-
-    @Column(nullable = false)
     private int rating;
 
     public RatingEntry() {}
@@ -31,32 +19,31 @@ public class RatingEntry {
         return id;
     }
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getMovieId() {
-        return movieId;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
     }
 
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    public String getMovieId() {
+        return movieId;
+    }
+
     public void setMovieId(String movieId) {
         this.movieId = movieId;
+    }
+
+    public int getRating() {
+        return rating;
     }
 
     public void setRating(int rating) {
         this.rating = rating;
     }
-
 }
---
+++ ratings-data-service/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS ratings (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    movie_id VARCHAR(255) NOT NULL,
+    rating INT NOT NULL
+);
