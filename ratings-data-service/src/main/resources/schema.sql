7e0e5a11ce6b5537cb809571bc22bf776c65d878
Ayman Algamal <ayman01287985950@gmail.com>
2025-03-08 00:48:29 +0200

    populate mysql

--- ratings-data-service/src/main/resources/schema.sql
+++ ratings-data-service/src/main/resources/schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS ratings (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     movie_id VARCHAR(255) NOT NULL,
-    rating INT NOT NULL
+    rating INT NOT NULL,
+    UNIQUE KEY unique_user_movie (user_id, movie_id)
 );
